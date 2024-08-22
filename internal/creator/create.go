e0a3e74907503f204e4b2a3581e9d8c80384955c
Digvijay Mahapatra <thediggu@gmail.com>
2024-08-23 01:25:47 +0530

    feat: Do a little refactor

---
+++ internal/creator/create.go
@@ -0,0 +1,106 @@
+package creator
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+)
+
+func getRandNumber(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func getRandTemperature() int {
+	return getRandNumber(-99, 99)
+}
+
+func getRandCityIndex() int {
+	return getRandNumber(1, 9998)
+}
+
+func getCitiesList() []string {
+	file, err := os.OpenFile("cities.txt", os.O_RDONLY, 0666)
+	if err != nil {
+		log.Fatal(fmt.Errorf("could not open cities file: %m", err))
+	}
+
+	cities := []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		city := scanner.Text()
+		cities = append(cities, city)
+	}
+
+	return cities
+}
+
+type Processor struct {
+	writer *csv.Writer
+	file   *os.File
+	wg     *sync.WaitGroup
+	mu     sync.Mutex
+}
+
+func appendString(processor *Processor, data [][]string) {
+	defer processor.wg.Done()
+
+	processor.mu.Lock()
+	err := processor.writer.WriteAll(data)
+	if err != nil {
+		log.Fatal(fmt.Errorf("could not open cities file: %m", err))
+	}
+	processor.mu.Unlock()
+}
+
+func CreateData() {
+	csvFile, csvFileErr := os.OpenFile("1brc.csv", os.O_RDWR|os.O_CREATE|os.O_TRUNC|os.O_APPEND, 0666)
+	if csvFileErr != nil {
+		log.Fatal(fmt.Errorf("error reading file %m", csvFileErr))
+	}
+	defer csvFile.Close()
+
+	cities := getCitiesList()
+
+	waitGroup := &sync.WaitGroup{}
+	writer := csv.NewWriter(bufio.NewWriter(csvFile))
+
+	processor := Processor{
+		writer: writer,
+		file:   csvFile,
+		wg:     waitGroup,
+	}
+	defer processor.writer.Flush()
+
+	guard := make(chan struct{}, 100)
+	batchSize := 10000
+	batchData := make([][]string, 0, batchSize)
+
+	var data []string
+
+	for i := 0; i < 1_000_000_000; i++ {
+		cityIndex := getRandCityIndex()
+		temp := strconv.Itoa(getRandTemperature())
+		data = []string{cities[cityIndex], temp}
+
+		batchData = append(batchData, data)
+
+		if len(batchData) >= batchSize {
+			guard <- struct{}{}
+			processor.wg.Add(1)
+
+			go func(dataCopy [][]string) {
+				defer func() { <-guard }()
+				appendString(&processor, dataCopy)
+			}(batchData[:])
+
+			batchData = make([][]string, 0, batchSize)
+		}
+	}
+
+	processor.wg.Wait()
+}
--- main.go
+++ main.go
@@ -2,116 +2,37 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
 	"fmt"
-	"log"
-	"math/rand"
 	"os"
-	"strconv"
-	"sync"
 	"time"
+
+	"github.com/gitfudge0/1brc.git/internal/creator"
 )
 
-func getRandNumber(min, max int) int {
-	return min + rand.Intn(max-min+1)
-}
-
-func getRandTemperature() int {
-	return getRandNumber(-99, 99)
-}
-
-func getRandCityIndex() int {
-	return getRandNumber(1, 9998)
-}
-
-func getCitiesList() []string {
-	file, err := os.OpenFile("cities.txt", os.O_RDONLY, 0666)
-	if err != nil {
-		log.Fatal(fmt.Errorf("could not open cities file: %m", err))
-	}
-
-	cities := []string{}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		city := scanner.Text()
-		cities = append(cities, city)
-	}
-
-	return cities
-}
-
-type Processor struct {
-	writer *csv.Writer
-	file   *os.File
-	wg     *sync.WaitGroup
-	mu     sync.Mutex
-}
-
-func appendString(processor *Processor, data [][]string) {
-	defer processor.wg.Done()
-
-	processor.mu.Lock()
-	err := processor.writer.WriteAll(data)
-	if err != nil {
-		log.Fatal(fmt.Errorf("could not open cities file: %m", err))
-	}
-	processor.mu.Unlock()
-}
-
-func createData() {
-	csvFile, csvFileErr := os.OpenFile("1brc.csv", os.O_RDWR|os.O_CREATE|os.O_TRUNC|os.O_APPEND, 0666)
-	if csvFileErr != nil {
-		log.Fatal(fmt.Errorf("error reading file %m", csvFileErr))
-	}
-	defer csvFile.Close()
-
-	cities := getCitiesList()
-
-	waitGroup := &sync.WaitGroup{}
-	writer := csv.NewWriter(bufio.NewWriter(csvFile))
-
-	processor := Processor{
-		writer: writer,
-		file:   csvFile,
-		wg:     waitGroup,
-	}
-	defer processor.writer.Flush()
-
-	guard := make(chan struct{}, 100)
-	batchSize := 10000
-	batchData := make([][]string, 0, batchSize)
-
-	var data []string
-
-	for i := 0; i < 1_000_000_000; i++ {
-		cityIndex := getRandCityIndex()
-		temp := strconv.Itoa(getRandTemperature())
-		data = []string{cities[cityIndex], temp}
-
-		batchData = append(batchData, data)
-
-		if len(batchData) >= batchSize {
-			guard <- struct{}{}
-			processor.wg.Add(1)
-
-			go func(dataCopy [][]string) {
-				defer func() { <-guard }()
-				appendString(&processor, dataCopy)
-			}(batchData[:])
-
-			batchData = make([][]string, 0, batchSize)
-		}
-	}
-
-	processor.wg.Wait()
-}
-
 func main() {
-	startTime := time.Now()
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Create data")
+	fmt.Println("2. Solve problem")
+	fmt.Println("Enter choice: ")
+	fmt.Println("")
 
-	fmt.Println("Hold on while the file is being generated...")
-	createData()
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("done")
-	fmt.Printf("Time taken: %s\n", time.Since(startTime))
+	switch input, _ := reader.ReadString('\n'); input {
+	case "1":
+		startTime := time.Now()
+
+		fmt.Println("Hold on while the file is being generated...")
+		creator.CreateData()
+
+		fmt.Println("done")
+		fmt.Printf("Time taken: %s\n", time.Since(startTime))
+
+	case "2":
+		fmt.Println("Will solve")
+		os.Exit(0)
+	default:
+		fmt.Println("lol")
+		os.Exit(1)
+	}
 }
